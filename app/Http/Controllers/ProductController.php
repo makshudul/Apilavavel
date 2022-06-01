66b225c2c913a177e5d554384e3ea87e00309e4a
makshudul <shihab90.it@gmail.com>
2022-06-01 10:29:36 +0600

    productCollection resource modify

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -18,7 +18,7 @@ class ProductController extends Controller
      */
     public function index()
     {
-        return Product::all();
+        return ProductCollection::collection(Product::all());
     }
 
     /**
